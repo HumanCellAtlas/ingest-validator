324237224a52b0292f75df489fe61cfc6da9d6c7
Tony Burdett <tburdett@ebi.ac.uk>
2017-09-27 21:37:05 +0100

    reorganising validator to fail early with ETag fail, then reGET and rePUT

--- ingestapi.py
+++ ingestapi.py
@@ -1,4 +1,6 @@
 import glob, json, os, urllib, requests, logging, time
+import urlparse
+
 import config
 
 ENTITY_TYPE_LINKS = {
@@ -11,9 +13,10 @@ ENTITY_TYPE_LINKS = {
 }
 
 SEARCH_UUID_PATH = '/search/findByUuid?uuid='
+MAX_RETRIES = 3
+
 
 class IngestApi:
-
     def __init__(self, ingest_url=None):
         self.ingest_url = ingest_url
 
@@ -38,9 +41,131 @@ class IngestApi:
         entity_url = json.load(entity_response)['_links']['self']['href']
         return entity_url
 
-
     def get_resource_callback(self, callback_link):
         resource_url = self.ingest_url + callback_link
         resource_response = urllib.urlopen(resource_url)
         resource = json.load(resource_response)
         return resource
+
+    def start_validation(self, entity_path, document_type):
+        # will loop for MAX_RETRIES in case of error
+        updated = False
+        retries = 0
+
+        while retries < MAX_RETRIES:
+            # do a GET request to test if validating link is present
+            entity_url = urlparse.urljoin(self.ingest_url, entity_path)
+            entity_response = requests.get(entity_url)
+            etag = entity_response.headers['ETag']
+
+            metadata_document = json.loads(entity_response.text)
+
+            # check the response is a document we want to validate
+            if not self.is_eligible(metadata_document, document_type):
+                self.logger.debug("Target document isn't eligible for validation")
+                return
+
+            # check the response is indeed ready to start validating
+            if self.is_ready_to_validate(metadata_document):
+                if etag:
+                    # set the etag header so we get 412 if someone beats us to set validating
+                    self.headers['If-Match'] = etag
+                self.logger.debug(self.headers)
+
+                validating_link = metadata_document['_links']['validating']['href']
+                transition_response = requests.put(validating_link, data={}, headers=self.headers)
+
+                if transition_response.status_code != requests.codes.ok:
+                    self.logger.error(str(transition_response))
+                    retries +=1
+                    self.logger.info('retries: ' + str(retries))
+                else:
+                    updated = True
+                    break
+            else:
+                self.logger.info('Target document ' + str(entity_path) + ' is not ready to validate, ignoring')
+                updated = False
+                break
+        return updated
+
+    def simulate_validation(self, entity_path):
+        self.logger.info("Validating '" + entity_path + "'...")
+        time.sleep(10)
+        self.logger.info("Validation of '" + entity_path + "' done!")
+        return True
+
+    def end_validation(self, entity_path):
+        # will loop for MAX_RETRIES in case of error
+        updated = False
+        retries = 0
+
+        while retries < MAX_RETRIES:
+            # do a GET request to test if validating link is present
+            entity_url = urlparse.urljoin(self.ingest_url, entity_path)
+            entity_response = requests.get(entity_url)
+            etag = entity_response.headers['ETag']
+
+            metadata_document = json.loads(entity_response.text)
+
+            # check the response is indeed ready to start validating
+            if self.is_ready_to_validate(metadata_document):
+                if etag:
+                    # set the etag header so we get 412 if someone beats us to set validating
+                    self.headers['If-Match'] = etag
+                self.logger.debug(self.headers)
+
+                validating_link = metadata_document['_links']['valid']['href']
+                transition_response = requests.put(validating_link, data={}, headers=self.headers)
+
+                if transition_response.status_code != requests.codes.ok:
+                    self.logger.error(str(transition_response))
+                    retries +=1
+                    self.logger.info('retries: ' + str(retries))
+                else:
+                    updated = True
+                    break
+            else:
+                self.logger.info('Target document ' + str(entity_path) +
+                                 ' cannot be set as valid (maybe already finished?), ignoring')
+                updated = False
+                break
+        return updated
+
+    def is_eligible(self, metadata_document, document_type):
+        # check is we should validate this metadata_document
+        # for now, we only validate if the document has a uuid, and only files if they also have a cloudUrl
+        uuid = metadata_document['uuid']
+        if not uuid:
+            self.logger.info('No uuid, discarding message')
+            return False
+        else:
+            # uuid is set, so check if it's a file...
+            if document_type == 'FILE':
+                # ... and it is, so check whether cloud_url is set
+                file_cloud_url = metadata_document['cloudUrl']
+                if file_cloud_url:
+                    # this is a file with a set cloud_url, so can be validated
+                    return True
+                else:
+                    # if not, this isn't eligible
+                    self.logger.info("no cloudUrl, discarding....")
+                    return False
+            else:
+                # ... which it isn't so we can validate
+                return True
+
+    def is_ready_to_validate(self, metadata_document):
+        # test for presence of 'validating' link
+        resource_links = metadata_document['_links']
+        if 'validating' in resource_links:
+            return True
+        else:
+            return False
+
+    def is_validating(self, metadata_document):
+        # test for presence of 'valid' link
+        resource_links = metadata_document['_links']
+        if 'valid' in resource_links:
+            return True
+        else:
+            return False
--- validationprocessor.py
+++ validationprocessor.py
@@ -8,104 +8,19 @@ class ValidationProcessor:
 
     def run(self, message):
         params = json.loads(message)
-        self.logger.info('Received message. Callback link: ' + params['callbackLink'])
-         
-        metadata_document = self.ingest_api.get_resource_callback(params['callbackLink'])
-        
-        # decide what to do with document
-        document_validation_state = metadata_document['validationState'].upper()
-        if document_validation_state == 'DRAFT':
-            self._set_metadata_validating(params['callbackLink'], params['documentType'].upper(), params['documentId'])
-        elif document_validation_state == 'VALIDATING':
-            self._set_metadata_valid(params['callbackLink'], params['documentType'].upper(), params['documentId'])
-        elif document_validation_state == 'VALID':
-            self.logger.info('received a document for processing that was valid')
+
+        entity_link = params['callbackLink']
+        document_type = params['documentType'].upper()
+
+        self.logger.info('Received message. Callback link: ' + entity_link)
+
+        if self.ingest_api.start_validation(entity_link, document_type):
+            if self.ingest_api.simulate_validation(entity_link):
+                if self.ingest_api.end_validation(entity_link):
+                    self.logger.info("Completed validation of '" + entity_link + "'!")
+                else:
+                    self.logger.error("Validation failed for '" + entity_link + "': could not end validation")
+            else:
+                self.logger.error("Validation failed for '" + entity_link + "': could not simulate validation")
         else:
-            self.logger.info('received a document in an unexcepted validation state: ' + document_validation_state)
-            
-
-    # returns 0 if successfuly set the document to 'validating'
-    # returns 1 if does not need to set document to 'validating' because it's already been set to 'validating' or had no uuid/cloudUrl
-    # returns -1 if failed to set document to 'validating'
-    def _set_metadata_validating(self, callback_link, metadata_type, metadata_id):
-        metadata_document = self.ingest_api.get_resource_callback(callback_link)
-
-        # if it's a file and doesn't have a cloudUrl then leave it in DRAFT
-        if metadata_type == 'FILE':
-            file_cloud_url = metadata_document['cloudUrl']
-            if file_cloud_url == None or file_cloud_url == '':
-                self.logger.info("no cloudUrl, discarding....")
-                return 1
-
-
-        # if it isn't accessioned (i.e has a uuid), wait til it does
-        uuid = metadata_document['uuid']
-        if (uuid == None) or (not 'uuid' in uuid) or (uuid['uuid'] == ''):
-            self.logger.info("no uuid, discarding....")
-            return 1
-
-        poll_count = 5
-        for i in range(0, poll_count):
-
-            resource_links = metadata_document['_links']
-            if 'validating' in resource_links:
-                set_resource_validating_url = resource_links['validating']['href']
-                r = requests.put(set_resource_validating_url, data={}, headers=self.headers)
-                sc = r.status_code
-                if sc == requests.codes.ok or sc == requests.codes.accepted:
-                    self.logger.info('successfully set document to validating')
-                    return 0
-                else:
-                    self.logger.info('status code ' + str(sc) + ' when trying to set document to validating')
-                    # if the code was 409, then maybe another validator did it?
-                    if sc == requests.codes.conflict:
-                        metadata_document = self.ingest_api.get_resource_callback(callback_link)
-                        resource_links = metadata_document['_links']
-                        if not 'validating' in resource_links:
-                            # assume another validator set it to 'validating'
-                            return 1
-            else:
-                time.sleep(0.5)
-
-        self.logger.error('failed to put metadata document into VALIDATING state after ' + str(poll_count) + ' attempts')
-        return -1
-
-
-    # returns 0 if successfuly set the document to 'valid'
-    # returns 1 if does not need to set document to 'valid' because it's already been set to 'valid'
-    # returns -1 if failed to set document to 'valid'
-    def _set_metadata_valid(self, callback_link, metadata_type, metadata_id):
-        metadata_document = self.ingest_api.get_resource_callback(callback_link)
-
-        poll_count = 5
-        for i in range(0, poll_count):
-
-            resource_links = metadata_document['_links']
-            if 'valid' in resource_links: # i.e we can now validate
-                set_resource_valid_url = resource_links['valid']['href']
-                r = requests.put(set_resource_valid_url, data={}, headers=self.headers)
-                sc = r.status_code
-                if sc == requests.codes.ok or sc == requests.codes.accepted:
-                    self.logger.info(str(r))
-                    return 0
-                else:
-                    self.logger.info('status code ' + str(sc) + ' when trying to set document to valid')
-                    # if the code was 409, then maybe another validator did it?
-                    if sc == requests.codes.conflict: 
-                        metadata_document = self.ingest_api.get_resource_callback(callback_link)
-                        resource_links = metadata_document['_links']
-                        if not 'validating' in resource_links:
-                            # assume another validator set it to 'validating'
-                            return 1
-            else:
-                time.sleep(0.5)
-
-        self.logger.error('failed to put metadata document into VALIDATING state after ' + str(poll_count) + ' attempts')
-        return -1
-
-
-    def _get_checksum(self, string):
-        
-        m = hashlib.md5()
-        m.update(string)
-        return m.hexdigest()
+            self.logger.error("Validation failed for '" + entity_link + "': could not start validation")
