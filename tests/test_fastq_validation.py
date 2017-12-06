487fd97c38d7d72825b3dabeb1be88e7802d2800
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2017-12-06 14:07:11 +0000

    Formatted code to use 4-space indentation.

--- tests/test_fastq_validation.py
+++ tests/test_fastq_validation.py
@@ -3,48 +3,39 @@ import os
 
 from validator.fastq import Validator
 
+
 class TestFastqFileValidation(unittest.TestCase):
-  #### single record tests###
-  def test_validates_ascii(self):
-    validator = Validator()
 
-    results = validator.validate(os.path.abspath("test_files/fastq/valid_ascii.fastq"))
+    #### single record tests###
 
-    self.assertTrue(results)
+    def test_validates_ascii(self):
+        validator = Validator()
+        results = validator.validate(os.path.abspath("test_files/fastq/valid_ascii.fastq"))
+        self.assertTrue(results)
 
-  def test_fails_with_invalid_ascii(self):
-    validator = Validator()
+    def test_fails_with_invalid_ascii(self):
+        validator = Validator()
+        results = validator.validate("test_files/fastq/invalid_ascii.fastq")
+        self.assertFalse(results)
 
-    results = validator.validate("test_files/fastq/invalid_ascii.fastq")
+    def test_correct_number_of_lines_and_valid_ascii(self):
+        validator = Validator()
+        results = validator.validate("test_files/fastq/correct_num_lines_valid_ascii.fastq")
+        self.assertTrue(results)
 
-    self.assertFalse(results)
+    def test_no_plus_char_on_third_line(self):
+        validator = Validator()
+        results = validator.validate("test_files/fastq/no_plus_char.fastq")
+        self.assertFalse(results)
 
-  def test_correct_number_of_lines_and_valid_ascii(self):
-    validator = Validator()
+    def test_no_ampersand_as_first_char(self):
+        validator = Validator()
+        results = validator.validate("test_files/fastq/no_at_symbol_first_line.fastq")
+        self.assertFalse(results)
 
-    results = validator.validate("test_files/fastq/correct_num_lines_valid_ascii.fastq")
+    ### multiple record tests###
 
-    self.assertTrue(results)
-
-  def test_no_plus_char_on_third_line(self):
-    validator = Validator()
-
-    results = validator.validate("test_files/fastq/no_plus_char.fastq")
-
-    self.assertFalse(results)
-
-  def test_no_ampersand_as_first_char(self):
-    validator = Validator()
-
-    results = validator.validate("test_files/fastq/no_at_symbol_first_line.fastq")
-
-    self.assertFalse(results)
-
-  ### multiple record tests###
-
-  def test_validates_ascii_multiple_records(self):
-    validator = Validator()
-
-    results = validator.validate(os.path.abspath("test_files/fastq/big.fastq"))
-
-    self.assertTrue(results)
+    def test_validates_ascii_multiple_records(self):
+        validator = Validator()
+        results = validator.validate(os.path.abspath("test_files/fastq/big.fastq"))
+        self.assertTrue(results)
