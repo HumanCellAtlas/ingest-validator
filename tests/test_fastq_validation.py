78c10df18aba44f0376231ed9d8b8d6ca0023824
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2017-12-06 16:19:34 +0000

    Updated test code to use setup validator variable.

--- tests/test_fastq_validation.py
+++ tests/test_fastq_validation.py
@@ -1,5 +1,5 @@
-import unittest
 import os
+import unittest
 
 from validator.fastq import Validator
 
@@ -13,40 +13,33 @@ class TestFastqFileValidation(unittest.TestCase):
     #### single record tests###
 
     def test_validates_ascii(self):
-        validator = Validator()
-        results = validator.validate(os.path.abspath("test_files/fastq/valid_ascii.fastq"))
+        results = self.validator.validate(os.path.abspath("test_files/fastq/valid_ascii.fastq"))
         self.assertTrue(results)
 
     def test_fails_with_invalid_ascii(self):
-        validator = Validator()
-        results = validator.validate("test_files/fastq/invalid_ascii.fastq")
+        results = self.validator.validate("test_files/fastq/invalid_ascii.fastq")
         self.assertFalse(results)
 
     def test_correct_number_of_lines_and_valid_ascii(self):
-        validator = Validator()
-        results = validator.validate("test_files/fastq/correct_num_lines_valid_ascii.fastq")
+        results = self.validator.validate("test_files/fastq/correct_num_lines_valid_ascii.fastq")
         self.assertTrue(results)
 
     def test_no_plus_char_on_third_line(self):
-        validator = Validator()
-        results = validator.validate("test_files/fastq/no_plus_char.fastq")
+        results = self.validator.validate("test_files/fastq/no_plus_char.fastq")
         self.assertFalse(results)
 
     def test_no_ampersand_as_first_char(self):
-        validator = Validator()
-        results = validator.validate("test_files/fastq/no_at_symbol_first_line.fastq")
+        results = self.validator.validate("test_files/fastq/no_at_symbol_first_line.fastq")
         self.assertFalse(results)
 
     def test_big_record(self):
-        validator = Validator()
-        results = validator.validate('test_files/fastq/big.fastq')
+        results = self.validator.validate('test_files/fastq/big.fastq')
         self.assertTrue(results)
 
     ### multiple record tests###
 
     def test_validates_ascii_multiple_records(self):
-        validator = Validator()
-        results = validator.validate(os.path.abspath("test_files/fastq/multiple_valid-ascii.fastq"))
+        results = self.validator.validate(os.path.abspath("test_files/fastq/multiple_valid-ascii.fastq"))
         self.assertTrue(results)
 
     def test_invalid_multiple_records(self):
