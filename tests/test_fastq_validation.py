3ab6a31978b5ed5ea648c669c0b33bd2883f4e5f
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2017-12-06 13:51:45 +0000

    Restructured code for FASTQ validation.

--- tests/test_fastq_validation.py
+++ tests/test_fastq_validation.py
@@ -1,42 +1,42 @@
 import unittest
 import os
 
-from validator import Validator
+from validator.fastq import Validator
 
 class TestFastqFileValidation(unittest.TestCase):
   #### single record tests###
   def test_validates_ascii(self):
     validator = Validator()
 
-    results = validator.validate(os.path.abspath("data/valid_ascii.fastq"))
+    results = validator.validate(os.path.abspath("test_files/fastq/valid_ascii.fastq"))
 
     self.assertTrue(results)
 
   def test_fails_with_invalid_ascii(self):
     validator = Validator()
 
-    results = validator.validate("data/invalid_ascii.fastq")
+    results = validator.validate("test_files/fastq/invalid_ascii.fastq")
 
     self.assertFalse(results)
 
   def test_correct_number_of_lines_and_valid_ascii(self):
     validator = Validator()
 
-    results = validator.validate("data/correct_num_lines_valid_ascii.fastq")
+    results = validator.validate("test_files/fastq/correct_num_lines_valid_ascii.fastq")
 
     self.assertTrue(results)
 
   def test_no_plus_char_on_third_line(self):
     validator = Validator()
 
-    results = validator.validate("data/no_plus_char.fastq")
+    results = validator.validate("test_files/fastq/no_plus_char.fastq")
 
     self.assertFalse(results)
 
   def test_no_ampersand_as_first_char(self):
     validator = Validator()
 
-    results = validator.validate("data/no_at_symbol_first_line.fastq")
+    results = validator.validate("test_files/fastq/no_at_symbol_first_line.fastq")
 
     self.assertFalse(results)
 
@@ -45,6 +45,6 @@ class TestFastqFileValidation(unittest.TestCase):
   def test_validates_ascii_multiple_records(self):
     validator = Validator()
 
-    results = validator.validate(os.path.abspath("data/big.fastq"))
+    results = validator.validate(os.path.abspath("test_files/fastq/big.fastq"))
 
     self.assertTrue(results)
