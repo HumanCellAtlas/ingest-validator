d1f558794f75f12e09a2882324ea5aa2cfc3bb18
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2017-12-23 01:13:27 +0000

    Added test for invalid report.

--- tests/test_validation_report.py
+++ tests/test_validation_report.py
@@ -7,11 +7,23 @@ class TestValidationReport(TestCase):
 
     def test_from_job_results_dict(self):
         # given:
-        report_map = { 'validation_state': 'VALID', 'validation_errors': [] }
+        report_map_valid = { 'validation_state': 'VALID', 'validation_errors': [] }
+
+        # and:
+        error_message = 'validation error'
+        report_map_invalid = {  'validation_state': 'INVALID', 'validation_errors': [
+            { 'user_friendly_message': error_message}
+        ] }
 
         # when:
-        report = ValidationReport.from_job_results_dict(report_map)
+        report_valid = ValidationReport.from_job_results_dict(report_map_valid)
+        report_invalid = ValidationReport.from_job_results_dict(report_map_invalid)
 
         # then:
-        self.assertEqual(report_map['validation_state'], report.validation_state)
-        self.assertEqual(0, len(report.error_reports))
+        self.assertEqual(report_map_valid['validation_state'], report_valid.validation_state)
+        self.assertEqual(0, len(report_valid.error_reports))
+
+        # and:
+        self.assertEqual(report_map_invalid['validation_state'], report_invalid.validation_state)
+        self.assertEqual(1, len(report_invalid.error_reports))
+        self.assertEqual(error_message, report_invalid.error_reports[0].message)
