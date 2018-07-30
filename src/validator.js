2935aabe6d8723e4bfc3879c6e1e0b26c601772f
Rolando Fernandez <rolando@ebi.ac.uk>
2018-07-30 17:13:24 +0100

    draft-04 support,  following and asynchronous schema compilation

--- src/validator.js
+++ src/validator.js
@@ -6,7 +6,8 @@ let IsValidTerm = require("./custom/isvalidterm");
 const ValidationError = require("./model/validation-error");
 const AppError = require("./model/application-error");
 
-let ajv = new Ajv({allErrors: true});
+let ajv = new Ajv({allErrors: true, schemaId: 'auto', loadSchema: loadSchemaRef});
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
 let isChildTermOf = new IsChildTermOf(ajv);
 let isValidTerm = new IsValidTerm(ajv);
 
@@ -28,24 +29,29 @@ function convertToValidationErrors(ajvErrorObjects) {
 function runValidation(inputSchema, inputObject) {
   logger.log("silly", "Running validation...");
   return new Promise((resolve, reject) => {
-    var validate = ajv.compile(inputSchema);
-    Promise.resolve(validate(inputObject))
-    .then((data) => {
-        if (validate.errors) {
-          logger.log("debug", ajv.errorsText(validate.errors, {dataVar: inputObject.alias}));
-          resolve(convertToValidationErrors(validate.errors));
+    var compiledSchemaPromise = ajv.compileAsync(inputSchema);
+    compiledSchemaPromise.then((validate) => {
+      Promise.resolve(validate(inputObject))
+        .then((data) => {
+              if (validate.errors) {
+                  logger.log("debug", ajv.errorsText(validate.errors, {dataVar: inputObject.alias}));
+                  resolve(convertToValidationErrors(validate.errors));
+              } else {
+                  resolve([]);
+              }
+          }
+        ).catch((err, errors) => {
+        if (!(err instanceof Ajv.ValidationError)) {
+            logger.log("error", "An error ocurred while running the validation.");
+            reject(new AppError("An error ocurred while running the validation."));
         } else {
-          resolve([]);
+            logger.log("debug", ajv.errorsText(err.errors, {dataVar: inputObject.alias}));
+            resolve(convertToValidationErrors(err.errors));
         }
-      }
-    ).catch((err, errors) => {
-      if (!(err instanceof Ajv.ValidationError)) {
-        logger.log("error", "An error ocurred while running the validation.");
-        reject(new AppError("An error ocurred while running the validation."));
-      } else {
-        logger.log("debug", ajv.errorsText(err.errors, {dataVar: inputObject.alias}));
-        resolve(convertToValidationErrors(err.errors));
-      }
+      });
+    }).catch((err) => {
+      console.log("async schema compiled encountered and error");
+      reject(err);
     });
   });
 }
@@ -67,14 +73,40 @@ function runAutoValidation(inputObject) {
             url: schemaUri,
             json: true
         };
-        request.options(reqOptions)
-            .on("response", (resp) => {
-              let inputSchema = resp.body;
-              return runValidation(inputSchema, inputObject)
-            })
-            .on("error", (err) => {
-              reject(new Error("Error retrieving schema at uri " + schemaUri + "; status code " + err.statusCode));
+        request(reqOptions, (err, resp, body) => {
+          if(resp.statusCode === 200) {
+            let inputSchema = resp.body;
+            inputSchema["$id"] = inputSchema["id"];
+            runValidation(inputSchema, inputObject).then((allGood) => {
+              resolve(allGood);
+            }).catch((err) => {
+              reject(err);
             });
+          } else {
+            reject(new Error("Error retrieving schema at uri " + schemaUri + "; status code " + err.statusCode));
+          }
+        });
     }
   });
 }
+
+function loadSchemaRef(uri) {
+    let reqOptions = {
+        method: "GET",
+        url: uri,
+        json: true
+    };
+    return new Promise((resolve, reject) => {
+        request(reqOptions, (err,resp) => {
+            if (resp.statusCode >= 400) {
+                reject(new Error('Loading error: ' + resp.statusCode));
+            } else {
+                resolve(resp.body);
+            }
+        });
+    });
+}
+
+module.exports = {};
+module.exports.runValidation = runValidation;
+module.exports.runAutoValidation = runAutoValidation;
