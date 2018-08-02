b3847dc5d063d25b79a86a13c014bef1aa8e817c
Rolando Fernandez <rolando@ebi.ac.uk>
2018-08-02 12:18:56 +0100

    generic rabbit listener with pluggable message handler

--- src/listener/document-update-listener.js
+++ src/listener/document-update-listener.js
@@ -1,3 +1,34 @@
 /**
  * Created by rolando on 01/08/2018.
  */
+const Listener = require('./listener');
+const Validator = require('../validation/validator');
+
+class Handler {
+    constructor(validator, ingestClient) {
+        this.validator = validator;
+        this.ingestClient = ingestClient;
+    }
+
+    handle(msg) {
+        let callbackLink = JSON.parse(msg.content)['callbackLink'];
+        // TODO; retrieve the document and maybe validate it
+    }
+}
+
+class DocumentUpdateListener {
+    constructor(rabbitUrl, exchange, queue) {
+        this.rabbitUrl = rabbitUrl;
+        this.exchange = exchange;
+        this.queue = queue;
+        this.handler = new Handler(Validator, null); // TODO; give the a handler an ingestClient
+        this.listener = new Listener(rabbitUrl, exchange, queue);
+        this.listener.setHandler(this.handler);
+    }
+
+    start(){
+        this.listener.start();
+    }
+}
+
+module.exports = DocumentUpdateListener;
---
+++ src/listener/listener.js
@@ -0,0 +1,40 @@
+/**
+ * Created by rolando on 02/08/2018.
+ */
+const amqp = require('amqplib');
+
+class Listener {
+    constructor(rabbitUrl, exchange, queue) {
+        this.rabbitUrl = rabbitUrl;
+        this.exchange = exchange;
+        this.queue = queue;
+    }
+
+    start(){
+        amqp.connect(this.rabbitUrl).then(conn => {
+            return conn.createChannel();
+        }).then(ch => {
+            ch.assertExchange(this.exchange, 'topic').then(() => {
+                ch.assertQueue(this.queue, {durable: false}).then(() => {
+                    ch.prefetch(1).then(() => {
+                        ch.consume(this.queue, (msg) => {
+                            this.handle(msg);
+                        }, {noAck : true});
+                    })
+                })
+            })
+
+        })
+    }
+
+    setHandler(handler) {
+        this.handler = handler;
+    }
+
+    handle(msg){
+        this.handler.handle(msg);
+    }
+
+}
+
+module.exports = Listener;
