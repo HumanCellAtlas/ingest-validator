696ea005640112b462b13c0d6ca6f0b64b7d1432
Rolando Fernandez <rolando@ebi.ac.uk>
2018-08-06 14:11:29 +0100

    modified exception class to enable bluebird catching

--- src/utils/ingest-client/ingest-client-exceptions.js
+++ src/utils/ingest-client/ingest-client-exceptions.js
@@ -2,11 +2,13 @@
  * Created by rolando on 06/08/2018.
  */
 
-class NoUuidError extends Error {
-    constructor(...args) {
-        super(...args);
-    }
+function NoUuidError(message="") {
+    this.message = message;
+    this.name = "NoUuidError";
+    Error.captureStackTrace(this, NoUuidError);
 }
+NoUuidError.prototype = Object.create(Error.prototype);
+NoUuidError.prototype.constructor = NoUuidError;
 
 module.exports = {
     "NoUuidError" : NoUuidError
