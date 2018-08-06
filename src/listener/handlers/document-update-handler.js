19e806de4c9ae5ec561b70317892965e155ae3b1
Rolando Fernandez <rolando@ebi.ac.uk>
2018-08-06 11:01:20 +0100

    fleshing out the document update event handler

--- src/listener/handlers/document-update-handler.js
+++ src/listener/handlers/document-update-handler.js
@@ -1,6 +1,8 @@
 /**
  * Created by rolando on 02/08/2018.
  */
+const Promise = require('bluebird');
+const NoUuidError = require('../../utils/ingest-client/ingest-client-exceptions').NoUuidError;
 
 class DocumentUpdateHandler {
     constructor(validator, ingestClient) {
@@ -10,7 +12,22 @@ class DocumentUpdateHandler {
 
     handle(msg) {
         let callbackLink = JSON.parse(msg.content)['callbackLink'];
-        // TODO; retrieve the document and maybe validate it
+
+        return new Promise((resolve, reject) => {
+            this.ingestClient.getMetadataDocument(callbackLink).then(doc => {
+                documentContent = doc["content"];
+                this.validator.autoValidate(documentContent).then(validationErrors => {
+                    resolve(validationErrors);
+                })
+            }).catch(NoUuidError,(err) => {
+                console.info("Document at " + callbackLink + " has no uuid, ignoring...");
+                reject(err);
+            }).catch(err => {
+                console.error("Error validating document with callback link " + callbackLink);
+                reject(err);
+            })
+        });
+
     }
 }
 
