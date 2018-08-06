cf100fbef336797d23755109db40e3d7c880d850
Rolando Fernandez <rolando@ebi.ac.uk>
2018-08-06 09:47:03 +0100

    added setDocumentState() and setValidationErrors() to the ingest client

--- src/utils/ingest-client.js
+++ src/utils/ingest-client.js
@@ -1,3 +1,59 @@
 /**
  * Created by rolando on 01/08/2018.
  */
+const request = require('request-promise');
+
+class IngestClient {
+    constructor(ingestUrl) {
+        this.ingestUrl = ingestUrl;
+    }
+
+    setDocumentState(entityCallback, validationState){
+        patchPayload = {
+          "validationState" : validationState
+        };
+
+        const entityUrl = this.urlFor(entityCallback);
+
+        return new Promise((resolve, reject) => {
+            request({
+                method: "PATCH",
+                url: entityUrl,
+                json: true,
+                data: patchPayload
+            }).then(resp => {
+                resolve(resp);
+            }).catch(err => {
+                reject(err);
+            })
+        });
+    }
+
+    setValidationErrors(entityCallback, validationErrors) {
+        patchPayload = {
+            "validationErrors" : validationErrors
+        };
+
+        const entityUrl = this.urlFor(entityCallback);
+
+        return new Promise((resolve, reject) => {
+            request({
+                method: "PATCH",
+                url: entityUrl,
+                json: true,
+                data: patchPayload
+            }).then(resp => {
+                resolve(resp);
+            }).catch(err =>{
+                reject(err);
+            });
+        });
+    }
+
+
+    urlFor(entityCallback) {
+        return this.ingestUrl + entityCallback;
+    }
+}
+
+module.exports = IngestClient;
