ce39008c898831dac45cc7ddd7cc48d4873b63d3
Rolando Fernandez <rolando@ebi.ac.uk>
2018-08-06 14:13:18 +0100

    extending Error for exception classes to enable bluebird filtering

--- src/utils/ingest-client/ingest-client-exceptions.js
+++ src/utils/ingest-client/ingest-client-exceptions.js
@@ -2,13 +2,7 @@
  * Created by rolando on 06/08/2018.
  */
 
-function NoUuidError(message="") {
-    this.message = message;
-    this.name = "NoUuidError";
-    Error.captureStackTrace(this, NoUuidError);
-}
-NoUuidError.prototype = Object.create(Error.prototype);
-NoUuidError.prototype.constructor = NoUuidError;
+class NoUuidError extends Error {}
 
 module.exports = {
     "NoUuidError" : NoUuidError
