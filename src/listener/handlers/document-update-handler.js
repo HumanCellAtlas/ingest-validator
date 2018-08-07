17247d9804f8215d14a7bdc50af26b3989ccc5d6
Rolando Fernandez <rolando@ebi.ac.uk>
2018-08-07 14:43:54 +0100

    added file+cloudUrl check, + refactoring

--- src/listener/handlers/document-update-handler.js
+++ src/listener/handlers/document-update-handler.js
@@ -4,6 +4,8 @@
 const Promise = require('bluebird');
 const NoUuidError = require('../../utils/ingest-client/ingest-client-exceptions').NoUuidError;
 
+class NoCloudUrl extends Error {}
+
 class DocumentUpdateHandler {
     constructor(validator, ingestClient) {
         this.validator = validator;
@@ -11,26 +13,39 @@ class DocumentUpdateHandler {
     }
 
     handle(msg) {
+        const msgJson = JSON.parse(msg.content);
 
-        const documentUrl = this.ingestClient.urlForCallbackLink(JSON.parse(msg.content)['callbackLink']);
+        const callbackLink = msgJson['callbackLink'];
+        const documentUrl = this.ingestClient.urlForCallbackLink(callbackLink);
+        const documentType = msgJson['documentType'].toUpperCase();
 
         return new Promise((resolve, reject) => {
-            this.ingestClient.getMetadataDocument(documentUrl).then(doc => {
-                const documentContent = doc["content"];
-                this.validator.autoValidate(documentContent).then(validationErrors => {
-                    this.ingestClient.setValidationErrors(documentUrl, validationErrors).then(resp => {
-                        console.info("Patched validation errors to document at " + documentUrl);
-                        resolve(resp);
-                    })
-                }).catch(err => {
-                    console.error("Error validating document with at " + documentUrl);
-                    reject(err);
-                })
-            }).catch(NoUuidError, (err) => {
-                console.info("Document at " + documentUrl + " has no uuid, ignoring...");
-            }).catch(err => {
-                console.error(err);
-            });
+            this.ingestClient.getMetadataDocument(documentUrl)
+                .then(doc => {return this.checkForCloudUrl(doc, documentType)})
+                .then(doc => {return this.validator.autoValidate(doc['content'])})
+                .then(validationErrors => {return this.ingestClient.setValidationErrors(documentUrl, validationErrors)})
+                .then(resp => resolve(resp))
+                .catch(NoCloudUrl, err => console.info("File document at " + documentUrl + "has no cloudUrl, ignoring.."))
+                .catch(NoUuidError, err => console.info("Document at " + documentUrl + " has no uuid, ignoring..."))
+                .catch(err => reject(err));
+        });
+
+    }
+
+    /**
+     * check if the document is a File and if so does an existence check for an assigned cloudUrl.
+     * returns/resolves the document in question if it passes the check, else throws/rejects-with an exception
+     *
+     * @param document
+     * @param documentType
+     */
+    checkForCloudUrl(document, documentType) {
+        return new Promise((resolve, reject) => {
+            if(documentType === 'FILE' && !document['cloudUrl']) {
+                reject(new NoCloudUrl());
+            } else {
+                resolve(document);
+            }
         });
     }
 }
