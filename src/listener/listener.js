1c7d9868884a6b19deda220d7f673937e48c71aa
Rolando Fernandez <rolando@ebi.ac.uk>
2018-08-07 13:35:20 +0100

    listener now accepts an exchange type parameter in its constructor

--- src/listener/listener.js
+++ src/listener/listener.js
@@ -4,18 +4,19 @@
 const amqp = require('amqplib');
 
 class Listener {
-    constructor(rabbitConnectionProperties, exchange, queue) {
+    constructor(rabbitConnectionProperties, exchange, queue, exchangeType) {
         this.rabbitConnectionProperties = rabbitConnectionProperties;
         this.rabbitUrl = rabbitConnectionProperties["scheme"] + "://" + rabbitConnectionProperties["host"] + ":" + rabbitConnectionProperties["port"];
         this.exchange = exchange;
         this.queue = queue;
+        this.exchangeType = exchangeType;
     }
 
     start(){
         amqp.connect(this.rabbitUrl).then(conn => {
             return conn.createChannel();
         }).then(ch => {
-            ch.assertExchange(this.exchange, 'topic').then(() => {
+            ch.assertExchange(this.exchange, this.exchangeType).then(() => {
                 ch.assertQueue(this.queue, {durable: false}).then(() => {
                     ch.prefetch(1).then(() => {
                         ch.consume(this.queue, (msg) => {
