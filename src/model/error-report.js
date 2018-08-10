b44ad3dac0b0aef25ff765224a99e42197e82a83
Rolando Fernandez <rolando@ebi.ac.uk>
2018-08-10 14:13:12 +0100

    ingest-format error report

--- src/model/error-report.js
+++ src/model/error-report.js
@@ -2,16 +2,32 @@
  * Created by rolando on 07/08/2018.
  */
 
-class ErrorReport{
-    constructor(userFriendlyMessage="") {
-        this._userFriendlyMessage = userFriendlyMessage;
+class ErrorReport {
+    constructor(ajvError=null) {
+        this.ajvError = ajvError;
+        this.message = null;
+        this.absoluteDataPath = null;
+        this.userFriendlyMessage = null;
+
+        if(ajvError) {
+            this.constructWithAjvError(this.ajvError);
+        }
     }
 
-    get userFriendlyMessage() {
-        return this._userFriendlyMessage;
+    constructUserFriendlyMessage() {
+        if(!this.absoluteDataPath) {
+            throw new Error("Can't construct a user friendly message: absoluteDataPath of error not set");
+        } else if(!this.message) {
+            throw new Error("Can't construct a user friendly message: error message not set");
+        } else {
+            this.userFriendlyMessage = message + " at " + this.absoluteDataPath;
+        }
     }
 
-    set userFriendlyMessage(value) {
-        this._userFriendlyMessage = value;
+    constructWithAjvError(ajvError) {
+        this.absoluteDataPath = ajvError.dataPath;
+        this.message = ajvError.message;
+
+        this.constructUserFriendlyMessage();
     }
 }
