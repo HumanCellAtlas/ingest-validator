526d1e01ac3929ff41705663ce6319014b5abdc3
prabht <prabhat.totoo@gmail.com>
2020-10-19 16:07:58 +0100

    Fixed test

--- src/validation/ingest-validator.spec.ts
+++ src/validation/ingest-validator.spec.ts
@@ -3,7 +3,6 @@ import SchemaValidator from "./schema-validator";
 import IngestClient from "../utils/ingest-client/ingest-client";
 import Promise from "bluebird";
 import * as TypeMoq from "typemoq";
-import IngestFileValidator from "../utils/ingest-client/ingest-file-validator";
 import ValidationReport from "../model/validation-report";
 
 
@@ -17,21 +16,20 @@ describe("Ingest validator tests", () =>{
                 }
             }
         };
-        let format = IngestValidator.fileFormatFromFileResource(fileResource);
+        let format = fileResource['content']['file_core']['format'];
         expect(format).toBe("fastq.gz");
     });
 
     it("should return an INVALID ValidationReport when describedBy schema can't be retrieved", () => {
         const mockSchemaValidator: TypeMoq.IMock<SchemaValidator> = TypeMoq.Mock.ofType<SchemaValidator>();
         const mockIngestClient: TypeMoq.IMock<IngestClient> = TypeMoq.Mock.ofType<IngestClient>();
-        const mockFileValidator: TypeMoq.IMock<IngestFileValidator> = TypeMoq.Mock.ofType<IngestFileValidator>();
 
         const badUrl = "badUrl";
         mockIngestClient
             .setup(mockInstance => mockInstance.fetchSchema(TypeMoq.It.isValue(badUrl)))
             .returns(() => Promise.reject(new Error()));
 
-        const ingestValidator = new IngestValidator(mockSchemaValidator.object, mockFileValidator.object, mockIngestClient.object);
+        const ingestValidator = new IngestValidator(mockSchemaValidator.object, mockIngestClient.object);
         const documentErroneousDescribedBy: object = {
             "content": {
                 "describedBy": "badUrl"
